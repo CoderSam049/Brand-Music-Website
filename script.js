8115eb5ea0d9229b8c8b3ce601a568d878b3506c
Coder_Sam_049 <codewithsam049@gmail.com>
2024-06-30 10:04:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const ham= document.querySelector(".hamburger");
+const responsive= document.querySelector(".responsive");
+const cross= document.querySelector(".cross");
+
+ham.addEventListener('click',(e)=>{
+responsive.classList.add('responsive-nav');
+e.stopPropagation;
+}
+)
+
+cross.addEventListener("click",()=>{
+    responsive.classList.remove('responsive-nav');
+    
+  
+})
